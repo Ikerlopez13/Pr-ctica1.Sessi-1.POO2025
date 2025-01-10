010392058d0c8d7a437ca75178b97fd56e412812
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-01-10 10:52:41 +0100

    Add files via upload

---
+++ Practica1ÍkerLópez&&ÒscarPujol/src/Sessió1/FlorirJardi.java
@@ -0,0 +1,17 @@
+package Sessió1;
+
+import jconsole.JConsole;
+
+public class FlorirJardi {
+
+	enum Estat { S, N, F }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	 private static int llegirValor(int minim, int maxim, String text) {
+		 
+	 }
+
+}
