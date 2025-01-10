87889342b4671ca50b4fd8ccfeb42d5f2d0a06e1
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-01-10 12:21:14 +0100

    Update FlorirJardi.java

--- Practica1ÍkerLópez&&ÒscarPujol/src/Sessió1/FlorirJardi.java
+++ Practica1ÍkerLópez&&ÒscarPujol/src/Sessió1/FlorirJardi.java
@@ -1,17 +1,71 @@
 package Sessió1;
 
 import jconsole.JConsole;
+import java.awt.Color;
+import java.util.Random;
 
 public class FlorirJardi {
 
-	enum Estat { S, N, F }
-	
+	enum Estat { Sembrat, Nascut, Florit }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JConsole console = new JConsole (160,40);
+
+		int fila, columna;
+
+		String textFila, textColumna, textRegarFila, textRegarColumna;
+
+		textFila= "Indica quantes files té el taulell: ";
+		textColumna= "Indica quantes columnes té el taulell: ";
+		textRegarFila = "Indica la fila de la component a regar ";
+		textRegarColumna = "Indica la columna de la component a regar ";
+
+		fila = llegirValor(3, 10, textFila, console);
+		columna = llegirValor(3, 10, textColumna, console);
+
+		Estat jardi [][] = new Estat [fila][columna];
 
 	}
-	 private static int llegirValor(int minim, int maxim, String text) {
+
+	private static int llegirValor(int minim, int maxim, String text, JConsole console) {
+		int valor;
+		console.println(text);
+
+		console.setForegroundColor(Color.GREEN);
+		valor = console.readInt();
+		while (valor<3 || valor>10) {
+			console.setForegroundColor(Color.RED);
+			console.print("El valor entrat està fora de l'interval (ha d'estar entre 3 i 10)");
+			console.println();
+			console.setForegroundColor(Color.GREEN);
+			valor = console.readInt();
+		}
+		console.resetColor();
+		return valor;
+
+	}
+	
+	 private static int sembrarJardi(Estat jardi[][]) {
+		 int valor;
+		
+		 Random aleatori = new Random ();
 		 
+		 for (int i = 0; i<jardi.length;i++) {
+			 for (int j = 0; j<jardi[0].length;j++) {
+				valor = aleatori.nextInt(1,4);
+				if (valor == 1) {
+					jardi [i][j] = Estat.Sembrat;
+				}
+				else if (valor == 2) {
+					jardi [i][j] = Estat.Nascut;
+				}
+				else if (valor == 3) {
+					jardi [i][j] = Estat.Florit;
+				}
+			}
+		 }
+		 return jardi;
 	 }
 
 }
